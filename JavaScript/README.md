43c85ed1a5116592f375891c52a77d6124899fd0
mgoscar <mgoscar@gmail.com>
2022-05-06 10:26:38 -0500

    formating README file

--- JavaScript/README.md
+++ JavaScript/README.md
@@ -2,5 +2,8 @@
 * Dictionaries and Maps
     * **URL:** https://www.hackerrank.com/challenges/linkedin-practice-dictionaries-and-maps/problem
     * **file:** phoneBook.js
-
+* Flipping the Matrix
+    * **URL:** https://www.hackerrank.com/challenges/flipping-the-matrix
+    * **file:** flippingMatrix.js
+    * **Explanation:** https://docs.google.com/spreadsheets/d/1vGX8IO4o7UNO4ADxGrkittBIet6eIxm8Vq6sgkHmsyA/edit?usp=sharing
 
---
+++ JavaScript/flippingMatrix.js
@@ -0,0 +1,47 @@
+/*
+https://www.hackerrank.com/challenges/flipping-the-matrix
+
+If you need a graphical explanation to know how works this, please review this document:
+https://docs.google.com/spreadsheets/d/1vGX8IO4o7UNO4ADxGrkittBIet6eIxm8Vq6sgkHmsyA/edit?usp=sharing
+
+112   42   83  119
+ 56  125   56   49
+ 15   78  101   43
+ 62   98  114  108
+
+a1	b	b	a2
+c	d	d	c
+c	d	d	c
+a3	b	b	a4
+
+positions
+a1 = [ i ] [ j ]
+a2 = [ i ] [ size-1-j ]
+a3 = [ size-1-i ] [ j ]
+a4 = [ size-1-i ] [ size-1-j ]
+*/
+
+let input = [[112,42,83,119],[56,125,56,49],[15,78,101,43],[62,98,114,108]];
+//let input = [[517,37,380,3727],[3049,1181,2690,1587],[3227,3500,2665,383],[4041,2013,384,965]];
+console.log(flippingMatrix(input));
+
+
+function flippingMatrix(matrix) {
+    // Write your code here
+    let size = matrix.length; //size of matrix
+    let hSize = size/2; //half
+    let total = 0; //the sum of the maximal elements in the matrix's upper-left quadrant
+
+    for (let i=0; i<hSize; i++) {
+        for (let j=0; j<hSize; j++) {
+            // Obtain the numbers on the matrix corners
+            let a1=matrix[i][j];
+            let a2=matrix[i][size-1-j];
+            let a3=matrix[size-1-i][j];
+            let a4=matrix[size-1-i][size-1-j];
+            //console.log ('a1='+a1+' a2='+a2+' a3='+a3+' a4='+a4);
+            total += Math.max(a1,a2,a3,a4); // find the max number on the corners
+        }
+    }
+    return total; //result
+}
